fcf137fae19c80b9d01d02fce3fa7e6b6a45e4b0
Unicornelia <ez.kornelia@gmail.com>
2025-03-14 12:52:05 +0100

    package and config fixes

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,16 @@
-import { ReportHandler } from 'web-vitals';
+import type { CLSMetric, FCPMetric, FIDMetric, LCPMetric, TTFBMetric } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+type ReportHandler = (metric: CLSMetric | FCPMetric | FIDMetric | LCPMetric | TTFBMetric) => void;
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    import('web-vitals').then((webVitals) => {
+      webVitals.onCLS(onPerfEntry);
+      webVitals.onFID(onPerfEntry);
+      webVitals.onFCP(onPerfEntry);
+      webVitals.onLCP(onPerfEntry);
+      webVitals.onTTFB(onPerfEntry);
+    }).catch((error) => console.error('Error loading web-vitals:', error));
   }
 };
 
