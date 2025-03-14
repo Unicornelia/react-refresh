3237137d0219c3835c912f8289ae2aa2496da196
Unicornelia <ez.kornelia@gmail.com>
2025-03-14 12:52:28 +0100

    new component for goals adding

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import './App.css';
-import GoalList from "./components/GoalList/GoalList";
-import {Goals} from "./types";
-import NewGoal from "./components/NewGoal/NewGoal";
+import GoalList from './components/GoalList/GoalList';
+import { Goal, Goals } from './types';
+import NewGoal from './components/NewGoal/NewGoal';
 
 function App() {
-    const courseGoals: Goals = [
-        {id: 1, text: 'Finish Course'},
-        {id: 2, text: 'Learn main topics'},
-        {id: 3, text: 'Refresh knowledge'}
-    ];
+  const courseGoals: Goals = [
+    { id: 1, text: 'Finish Course' },
+    { id: 2, text: 'Learn main topics' },
+    { id: 3, text: 'Refresh knowledge' }
+  ];
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Yo</h1>
-                <NewGoal/>
-                <h2>Course goals:</h2>
-                <GoalList goals={courseGoals}/>
-            </header>
-        </div>
-    );
+  const addNewGoalHandler = (goal: Goal) => {
+    courseGoals.push(goal);
+    console.log(courseGoals);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Yo</h1>
+        <h2>Course goals:</h2>
+        <NewGoal onAddGoal={addNewGoalHandler} />
+        <GoalList goals={courseGoals} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/components/GoalList/GoalList.tsx
+++ src/components/GoalList/GoalList.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import './GoalList.css'
-import {Goal, Goals} from "../../types";
+import React from 'react';
+import './GoalList.css';
+import { Goal, Goals } from '../../types';
 
-const GoalList = ({goals}: {goals: Goals}) => {
-    return (
-        <ul className="goal-list">
-            {goals.map((goal: Goal) => (
-                <li key={goal.id}>{goal.text}</li>
-            ))}
-        </ul>
-    )
-}
+const GoalList = ({ goals }: { goals: Goals }) => {
+  return (
+    <ul className="goal-list">
+      {goals.map((goal: Goal) => (
+        <li key={goal.id}>{goal.text}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default GoalList;
